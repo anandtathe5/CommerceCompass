f7463261eb474988fa9fbe6b430e475ef5588c0e
anandtathe5 <166648054+anandtathe5@users.noreply.github.com>
2024-08-12 17:11:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Select the button by its ID
+document.getElementById('exploreButton').onclick = function() {
+    window.location.href = 'resources.html'; // Replace 'resources.html' with your target page
+};
+
+document.getElementById('connectWallet').onclick = function() {
+    window.location.href = 'wallet.html'; // Replace 'resources.html' with your target page
+};
